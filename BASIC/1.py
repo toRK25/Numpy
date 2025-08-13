780cb90032da816a4cb1c6485c1330123c55a925
VEDANT SINGH <vedantsingh591@gmail.com>
2025-08-13 18:46:16 +0530

    NUMPY BASICS

---
+++ BASIC/1.py
@@ -0,0 +1,19 @@
+import numpy as np
+
+#creating array from list
+arr1d = np.array([1,2,3,4])
+print("1D ARRAY",arr1d)
+
+arr2d = np.array([[1,2,3],[1,2,3]])
+print("2D ARRAY",arr2d)
+
+# --------------------
+
+#LIST VS ARRAY
+
+list1=[1,2,3]
+print("PYHTON LIST MULTIPLICATION :",list1*2)
+
+arr=np.array([1,2,3])
+print("PYHTON ARRAY MULTIPLICATION :",arr*2)
+
---
+++ BASIC/types.py
@@ -0,0 +1,15 @@
+#Types of Arrays in NUMPY
+import numpy as np
+
+#1ST TYPE
+zero=np.zeros((4,5))
+print("ZERO ARRAY : \n",zero)
+
+#2ND TYPE
+one=np.ones((3,5))
+print("ONES ARRAY : \n",one)
+
+#3rd TYPE-for printing constant numbers
+ 
+full=np.full((4,6),5)
+print("constant ARRAY : \n",full)
