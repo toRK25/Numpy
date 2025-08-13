15595a77462c155d573cd2b479bd4c91ec73c132
VEDANT SINGH <vedantsingh591@gmail.com>
2025-08-13 20:03:19 +0530

    Update Tensor.py

--- BASIC/Tensor.py
+++ BASIC/Tensor.py
@@ -1,3 +1,14 @@
 #tensor is basically a mulitdimensional array
 import numpy as np
 
+#vector 
+vec=np.array([1,2,3])
+print("vector \n",vec)
+
+#matrix
+mat= np.array([[1,2,3],[4,53,6]])
+print("MATRIX \n",mat)
+
+#TENSOR
+ten=np.array([[[1,2,3],[1,2,3]],[[4,5,6,],[7,6,5]]])
+print("TENSOR \n",ten)
