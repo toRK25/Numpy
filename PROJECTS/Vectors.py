6da2d40e64aa63ad6f2716af60f18ef2bfa64f80
VEDANT SINGH <vedantsingh591@gmail.com>
2025-08-16 13:10:12 +0530

    Create Vectors.py

---
+++ PROJECTS/Vectors.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+vec1=np.array([1,2,3,4,5,6])
+vec2=np.array([5,6,7,8,9,2])
+
+print("\n VECTOR ADDITION :", vec1 + vec2 )
+print("\n VECTOR SUBTRACTION :", vec1 - vec2 )
+print("\n VECTOR MULTIPLICATION :", vec1 * vec2 )
+print("\n VECTOR DIVISION :", vec1 / vec2 )
+print("\n VECTOR DOT PRODUCT :", np.dot(vec1, vec2))
